2b8c49d52b5e9e5b78cd584f553aa19e4eb30352
賴廷榛 <43640816+TimLai666@users.noreply.github.com>
2023-08-15 17:07:54 +0800

    Add files via upload

--- 股價預測.py
+++ 股價預測.py
@@ -1,13 +1,25 @@
+#v2
+
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import GRU, Dropout, Dense
+from tensorflow.keras.optimizers import Nadam
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.regularizers import l1_l2
+from tensorflow.keras.callbacks import ReduceLROnPlateau
 import yfinance as yf
 import pandas as pd
 import numpy as np
 import datetime
 from sklearn.preprocessing import MinMaxScaler
 import os
+
+def get_data_with_indicators(ticker):
+    df = get_data(ticker)
+    df = add_technical_indicators(df)
+    df.fillna(0, inplace=True)  # 填充 DataFrame 中的所有 NaN 值
+    return df
 
 def get_data(ticker):
     today = datetime.date.today()
@@ -31,31 +43,89 @@ def get_data(ticker):
     df["start_of_month"] = df["Date"].dt.is_month_start.astype(int)
     # 提取是否為月末
     df["end_of_month"] = df["Date"].dt.is_month_end.astype(int)
-    df["Amplitude(%)"] = (df['High'] / df["Open"] - df['Low'] / df["Open"]) * 100
+    df["Amplitude"] = df['High'] / df["Open"] - df['Low'] / df["Open"]
+    df["Change"] = (df["Close"] - df["Close"].shift(1))/df["Close"].shift(1)
+
     df.set_index(["Date"], inplace = True)
-    df.columns = ["Open", "High", "Low", "Close", "Adj Close", "Volume", 'Year', 'Year%4', 'Year%2', 'Month', 'Day', "weekday", "start_of_month", "end_of_month","Amplitude(%)"]
+    df.columns = ["Open", "High", "Low", "Close", "Adj Close", "Volume", 'Year', 'Year%4', 'Year%2', 'Month', 'Day', "weekday", "start_of_month", "end_of_month","Amplitude", "Change"]
+    return df
+
+def add_technical_indicators(df):
+    # 1. Calculate SMA
+    df['SMA_10'] = df['Close'].rolling(window=10).mean()
+    df['SMA_30'] = df['Close'].rolling(window=30).mean()
+    
+    # 2. Calculate EMA
+    df['EMA_10'] = df['Close'].ewm(span=10, adjust=False).mean()
+    df['EMA_30'] = df['Close'].ewm(span=30, adjust=False).mean()
+    
+    # 3. Calculate RSI
+    delta = df['Close'].diff()
+    gain = (delta.where(delta > 0, 0))
+    loss = (-delta.where(delta < 0, 0))
+    avg_gain = gain.rolling(window=14).mean()
+    avg_loss = loss.rolling(window=14).mean()
+    rs = avg_gain / avg_loss
+    df['RSI'] = 100 - (100 / (1 + rs))
+    
+    # 4. Calculate Momentum
+    df['Momentum'] = df['Close'] - df['Close'].shift(4)
+    
+    # 5. Calculate Bollinger Bands
+    df['Bollinger_Middle'] = df['Close'].rolling(window=20).mean()
+    df['Bollinger_Std'] = df['Close'].rolling(window=20).std()
+    df['Bollinger_Upper'] = df['Bollinger_Middle'] + (df['Bollinger_Std'] * 2)
+    df['Bollinger_Lower'] = df['Bollinger_Middle'] - (df['Bollinger_Std'] * 2)
+    
+    # 6. Calculate MACD
+    short_EMA = df['Close'].ewm(span=12, adjust=False).mean()
+    long_EMA = df['Close'].ewm(span=26, adjust=False).mean()
+    df['MACD'] = short_EMA - long_EMA
+    df['Signal_Line'] = df['MACD'].ewm(span=9, adjust=False).mean()
+    
+    # 7. Calculate Stochastic Oscillator
+    high_14 = df['High'].rolling(14).max()
+    low_14 = df['Low'].rolling(14).min()
+    df['%K'] = (df['Close'] - low_14) * 100 / (high_14 - low_14)
+    df['%D'] = df['%K'].rolling(window=3).mean()
+    
     return df
 
 def normalization(df):
-    # 數據正規化
+    # Splitting the data into train and test sets
+    train_size = int(0.8 * len(df))
+    train_data = df.iloc[:train_size]
+    test_data = df.iloc[train_size:]
+    
+    # Fit the scaler only on the training data
     scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(df.values)
+    scaled_train_data = scaler.fit_transform(train_data.values)
+    
+    # Transform the entire dataset using the scaler parameters from the training set
+    scaled_data = scaler.transform(df.values)
+    
     return scaled_data, scaler
 
-def build(scaled_data, ticker_symbol):
-    strategy = tf.distribute.MirroredStrategy()
+def constrained_mae(y_true, y_pred):
+    """ 
+    Constrained Mean Absolute Error Loss. 
+    This penalizes predictions that are outside the [-0.1, 0.1] range more heavily.
+    """
+    base_loss = tf.abs(y_true - y_pred)
+    penalty = tf.where(tf.logical_or(y_pred < -0.1, y_pred > 0.1), 1.0, 0.0)
+    return tf.reduce_mean(base_loss + penalty)
+
+def build(strategy, scaled_data, ticker_symbol):
     with strategy.scope():
         print("正在建立模型...")
-        # 模型參數
-        input_length = 500  # 假設使用過去500天的數據
-        input_dim = 15  # 根據您的數據集，有15個特徵
+        input_length = 500
+        input_dim = 30
 
-        # 創建訓練和測試數據
         X = []
         y = []
-        for i in range(input_length, len(scaled_data) - 4):  # -4 是為了確保有5天的數據
+        for i in range(input_length, len(scaled_data) - 14):
             X.append(scaled_data[i-input_length:i, :])
-            y.append(scaled_data[i:i+5, 3])  # 預測5天
+            y.append(scaled_data[i:i+15, 3])  # 直接使用收盤價格
 
         X, y = np.array(X), np.array(y)
 
@@ -63,10 +133,9 @@ def build(scaled_data, ticker_symbol):
         X_train, X_test = X[:train_size], X[train_size:]
         y_train, y_test = y[:train_size], y[train_size:]
 
-        # 建立模型
         model = Sequential()
         model.add(GRU(256, return_sequences=True, input_shape=(input_length, input_dim)))
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.2))
 
         num_layers = 10
         for _ in range(num_layers - 1):
@@ -75,56 +144,54 @@ def build(scaled_data, ticker_symbol):
 
         model.add(GRU(256))
         model.add(Dropout(0.5))
-        model.add(Dense(5))
+        model.add(Dense(15, activation='linear'))  # Directly predicting closing prices
 
-        model.compile(optimizer='adam', loss='mean_squared_error')
+        nadam_optimizer = Nadam(learning_rate=0.001, beta_1=0.9, beta_2=0.999)
+        model.compile(optimizer=nadam_optimizer, loss='mse')  # Using Mean Squared Error as the loss function
 
-        # 使用模型訓練
-        # 將數據轉為tf.data.Dataset來充分利用所有CPU核心
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.00001)
         train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train)).shuffle(len(X_train)).batch(128).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
         val_dataset = tf.data.Dataset.from_tensor_slices((X_test, y_test)).batch(128).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        early_stopping = EarlyStopping(patience=30, restore_best_weights=True)
+        model.fit(train_dataset, epochs=250, validation_data=val_dataset, callbacks=[early_stopping, reduce_lr])
 
-        model.fit(train_dataset, epochs=50, validation_data=val_dataset)
-
-    # 儲存模型
     save_dir = 'saved_models'
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     model_path = os.path.join(save_dir, ticker_symbol)
-    model_path = os.path.abspath(model_path)  # 獲取絕對路徑
+    model_path = os.path.abspath(model_path)
     model.save(model_path)
 
 def predict(model, scaled_data, scaler):
     print("正在使用模型預測...")
-    # 假設您想使用最新的500天數據進行預測
     x_latest = scaled_data[-500:]
-    x_latest = np.array([x_latest])  # 將其轉換為模型所需的形狀
-    predicted_values = model.predict(x_latest)
+    x_latest = np.array([x_latest])  
+    scaled_predictions = model.predict(x_latest)
     
-    # Create a dummy array with the same shape as predicted_values but with 10 extra columns to match the original number of features
-    dummy_array = np.zeros((predicted_values.shape[0], 15))
-    dummy_array[:, :5] = predicted_values
+    # Create a dummy array with the same shape as our dataset
+    dummy_array = np.zeros(shape=(len(scaled_predictions[0]), df.shape[1]))
     
-    # Use the scaler to inverse transform
-    inverse_transformed_values = scaler.inverse_transform(dummy_array)
+    # Place our predictions in the 'Close' column of our dummy array
+    dummy_array[:, 3] = scaled_predictions[0]
     
-    # Extract only the predicted close prices
-    predicted_close_prices = inverse_transformed_values[:, :5]
-    return predicted_close_prices
+    # Use the inverse_transform method to decode our predicted prices
+    inverted_array = scaler.inverse_transform(dummy_array)
+    
+    # Return only the 'Close' column values (which now have our decoded prices)
+    return inverted_array[:, 3]
 
 def plot_predictions(ticker_symbol, df, predictions):
     latest = df.index[-1]
-    days = ["Latest", "Day 1", "Day 2", "Day 3", "Day 4", "Day 5"]
+    days = ["Latest"] + [f"Day {i}" for i in range(1, 16)]
     latest_price = float(df.loc[latest]["Close"])
     figurelist = [latest_price] + predictions[0].tolist()
-    plt.figure(figsize=(10,6))
+    plt.figure(figsize=(12,6))
     plt.plot(days, figurelist, marker='o', linestyle='-', color='b')
-    plt.title(f"[{ticker_symbol}] Predicted Closing Prices for Next 5 Days")
+    plt.title(f"[{ticker_symbol}] Predicted Closing Prices for Next 15 Days")
     plt.xlabel("Days")
     plt.ylabel("Predicted Price")
     plt.grid(True)
     
-    # Annotate each point with its corresponding value
     for i, txt in enumerate(figurelist):
         plt.annotate(f"{txt:.2f}", (days[i], figurelist[i]), textcoords="offset points", xytext=(0,10), ha='center')
     
@@ -134,7 +201,7 @@ def incremental_training(scaled_data, scaler, ticker_symbol, strategy):
     with strategy.scope():
         model_path = os.path.join('saved_models/', ticker_symbol)
         if os.path.exists(model_path):
-            model = tf.keras.models.load_model(model_path)
+            model = tf.keras.models.load_model(model_path, custom_objects={'constrained_mae': constrained_mae})
         else:
             raise ValueError("Model not found!")
         
@@ -147,31 +214,49 @@ def incremental_training(scaled_data, scaler, ticker_symbol, strategy):
 
         for i in range(start_index, len(scaled_data) - input_length):  # -input_length 是為了確保有足夠的數據進行訓練
             X.append(scaled_data[i:i+input_length, :])
-            y.append(scaled_data[i+input_length, 3])  # 只預測下一天
+            y.append(scaled_data[i+input_length, 3])  # 只預測下一天的收盤價
 
         X, y = np.array(X), np.array(y)
 
         # 使用模型訓練
         train_dataset = tf.data.Dataset.from_tensor_slices((X, y)).batch(128).prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
-        # 使用較低的學習率
-        optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
-        model.compile(optimizer=optimizer, loss='mean_squared_error')
+        # 加入早期停止
+        early_stopping = EarlyStopping(patience=30, restore_best_weights=True)
 
-        model.fit(train_dataset, epochs=200)  # 這裡我們假設只訓練200個迭代
+        # 使用較低的學習率
+        # 使用Nadam優化器
+        # Using Mean Squared Error as the loss function when recompiling
+        nadam_optimizer = Nadam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999)
+        model.compile(optimizer=nadam_optimizer, loss='mse')
+
+        model.fit(train_dataset, epochs=500, callbacks=[early_stopping])
 
     # 儲存模型
     model_path = os.path.join('saved_models/', ticker_symbol)
     model.save(model_path)
 
 def main():
-    strategy = tf.distribute.MirroredStrategy()
-    print(f"Number of GPUs: {strategy.num_replicas_in_sync}")
+    # 偵測TPU並創建TPU策略
+    try:
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # 這將在Colab中返回一個適當的TPU
+        print('Running on TPU ', tpu.master())
+    except ValueError:
+        tpu = None
+
+    if tpu:
+        tf.config.experimental_connect_to_cluster(tpu)
+        tf.tpu.experimental.initialize_tpu_system(tpu)
+        strategy = tf.distribute.experimental.TPUStrategy(tpu)
+        print(f"Number of TPU cores: {strategy.num_replicas_in_sync}")
+    else:
+        strategy = tf.distribute.MirroredStrategy()  # 如果沒有TPU，則繼續使用GPU策略
+        print(f"Number of GPUs: {strategy.num_replicas_in_sync}")
 
     # 設定ETF代碼
     ticker_symbol = str(input("輸入要預測的股票代碼："))
     ticker = ticker_symbol+".TW"
-    df = get_data(ticker)
+    df = get_data_with_indicators(ticker)
     scaled_data, scaler = normalization(df)
 
     model_path = os.path.join('saved_models/', ticker_symbol)
@@ -180,8 +265,8 @@ def main():
         print("載入已訓練的模型...")
     else:
         print("建立新模型...")
-        build(scaled_data, ticker_symbol)
-    model = tf.keras.models.load_model(model_path)
+        build(strategy, scaled_data, ticker_symbol)
+    model = tf.keras.models.load_model(model_path, custom_objects={'constrained_mae': constrained_mae})
     predictions = predict(model, scaled_data, scaler)
         
     # 印出五天的預測值
